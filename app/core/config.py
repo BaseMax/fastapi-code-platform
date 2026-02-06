e100ba6dbcc2971b923476d250bc8a7e659ad90d
Seyyed Ali Mohammadiyeh (MAX BASE) <MaxBaseCode@gmail.com>
2026-02-06 05:16:59 +0330

    change config to run project locally out of docker

--- app/core/config.py
+++ app/core/config.py
@@ -2,8 +2,8 @@ from pydantic import BaseModel
 
 
 class Settings(BaseModel):
-    DB_URL: str = "mysql+aiomysql://app:app@mariadb:3306/code"
-    REDIS_URL: str = "redis://redis:6379/0"
+    DB_URL: str = "mysql+aiomysql://root:rootpass@localhost:3306/code"
+    REDIS_URL: str = "redis://localhost:6379/0"
     SANDBOX_CONTAINER: str = "code-sandbox"
     EXEC_TIMEOUT: int = 5
     MAX_OUTPUT: int = 20000
